f73f0b76c05fb753b6a596660ff0a4b93c967419
Scott Greff <scottiegreff@gmail.com>
2024-07-11 10:42:53 -0700

    add welcome messsage when user signs up to db

--- actions/postEmail.js
+++ actions/postEmail.js
@@ -4,13 +4,12 @@ import { db } from "@/lib/firebase.js";
 import { addDoc, collection } from "firebase/firestore";
 
 const postEmail = async (formData) => {
-  console.log("formData !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", formData);
   const collectionRef = collection(db, "users");
 
   const docRef = await addDoc(collectionRef, {
     email: formData.email,  
   });
-  console.log("Document written with ID: ", docRef);
+  return
 };
 
 export default postEmail;
--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,15 +1,12 @@
 "use client";
 
 import * as z from "zod";
-// import axios from "axios";
 import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
 
 import postEmail from "@/actions/postEmail";
-// import { useParams, useRouter } from "next/navigation";
-
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import {
@@ -20,8 +17,6 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import { Label } from "@/components/ui/label";
-// import { Heading } from "@/components/ui/heading";
 
 const formSchema = z.object({
   email: z.string().email(),
@@ -31,10 +26,11 @@ type CustomerFormValues = z.infer<typeof formSchema>;
 
 export const ContactForm: React.FC = () => {
   const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState(false);
 
   const title = "Create customer";
   const description = "Add a new customer";
-  const toastMessage = "Customer created.";
+  const toastMessage = "Thank you, Scott will be in contact with you shortly!";
   const action = "Connect";
   const loadingAction = "Connecting";
 
@@ -48,9 +44,11 @@ export const ContactForm: React.FC = () => {
   const onSubmit = async (data: CustomerFormValues) => {
     setLoading(true);
     try {
-      const res = await postEmail(data);
-      console.log("data", data);
-      
+      await postEmail(data);
+      setUser(true);
+      setTimeout(() => {
+        toast.success(toastMessage);
+      }, 3000);
     } catch (error: any) {
       toast.error("Something went wrong.");
     } finally {
@@ -60,53 +58,44 @@ export const ContactForm: React.FC = () => {
 
   return (
     <>
-      {/* <AlertModal
-        isOpen={open}
-        onClose={() => setOpen(false)}
-        onConfirm={onDelete}
-        loading={loading}
-      /> */}
-      {/* <div className="flex items-center justify-between">
-        <Heading title={title} description={description} />
-  
-      </div> */}
-
-      {/* CUST FIRST NAME */}
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="space-y-4 text-red-700 ms-3"
-        >
-          <div className="">
-            {/* EMAIL */}
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Email</FormLabel>
-                  <FormControl>
-                    <Input
-                      disabled={loading}
-                      className="bg-white w-60 md:w-72"
-                      placeholder=""
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
-          <Button
-            disabled={loading}
-            className="ml-auto bg-red-700 drop-shadow-xl"
-            type="submit"
+      {user ? (
+        <div className="ms-3 text-red-700 font-semibold">{toastMessage}</div>
+      ) : (
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-4 text-red-700 ms-3"
           >
-            {loading ? loadingAction : action}
-          </Button>
-        </form>
-      </Form>
+            <div>
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={loading}
+                        className="bg-white w-60 md:w-72"
+                        placeholder=""
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <Button
+              disabled={loading}
+              className="ml-auto bg-red-700 drop-shadow-xl"
+              type="submit"
+            >
+              {loading ? loadingAction : action}
+            </Button>
+          </form>
+        </Form>
+      )}
       <p className="ms-3 my-3 text-red-700">scottiegreff@gmail.com</p>
     </>
   );
